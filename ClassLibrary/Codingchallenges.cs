680fbf79c3fdc10916c295f1b636530897da2e75
GT <gaetan23@msn.com>
2019-05-06 18:51:17 -0400

    Added simple adding.

--- ClassLibrary/Codingchallenges.cs
+++ ClassLibrary/Codingchallenges.cs
@@ -84,15 +84,14 @@ namespace ClassLibrary
 //        https://www.coderbyte.com/editor/guest:Simple%20Adding:Csharp
         public static int SimpleAdding(int num)
         {
+            var total = 0;
 
-            // code goes here  
-            /* Note: In C# the return type of a function and the 
-               parameter types being passed are defined, so this return 
-               call must match the return type of the function.
-               You are free to modify the return type. */
-
-            return num;
-
+            for (int i = 1; i <= num; i++)
+            {
+                total += i;
+            }
+            
+            return total;
         }
     }
 }
--- CodeChallengeds/Program.cs
+++ CodeChallengeds/Program.cs
@@ -10,12 +10,14 @@ namespace CodeChallengeds
             var word = CodingChallenges.LongestWord("Hi!! How Are You??");
             var factorial = CodingChallenges.FirstFactorial(8);
             var reverse = CodingChallenges.FirstReverse("Hello World!");
-            var letterChange = CodingChallenges.LetterChanges("ZzzzZZZ");
+            var letterChange = CodingChallenges.LetterChanges("fun times!");
+            var simpleAdding = CodingChallenges.SimpleAdding(140);
 
             Console.WriteLine(word);
             Console.WriteLine(factorial);
             Console.WriteLine(reverse);
             Console.WriteLine(letterChange);
+            Console.WriteLine(simpleAdding);
 
             Console.ReadKey();
         }
